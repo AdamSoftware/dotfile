be5798ad4fd1c6ec6677fd0979f7e0970832e348
Adam Steinberg <adamsteinberg16@gmail.com>
2024-12-09 15:51:56 -0600

    delete this file

--- nvim/lua/adam/plugins-setup.lua
+++ nvim/lua/adam/plugins-setup.lua
@@ -291,6 +291,12 @@ return packer.startup(function(use)
 
 			-- Set up a keymap to open LazyGit in a floating window
 			vim.api.nvim_set_keymap("n", "<leader>gg", ":LazyGit<CR>", { noremap = true, silent = true })
+
+			-- Enable navigation using Ctrl + hjkl
+			vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
+			vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 		end,
 	})
 
